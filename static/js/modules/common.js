079d9303b5288e3b80a22a13daedc2c0fb911f7d
Andrew <drew.bohler@gmail.com>
2021-06-06 19:30:16 -0500

    added init function

--- static/js/modules/common.js
+++ static/js/modules/common.js
@@ -1,8 +1,16 @@
+import { init as textureInit } from "./texture.js";
+
 var gl;
+const STATE = new Map();
 
 function setContext(canvas)
 {
-    gl = canvas.getContext('webgl');
+    gl = canvas.getContext('webgl2');
 }
 
-export { gl, setContext };
+function init()
+{
+    textureInit();
+}
+
+export { gl, setContext, STATE, init };
